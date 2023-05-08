989b831fc3a4631b68b88653a7256cecbb970775
hheimerd <hheimerd@gmail.com>
2023-05-08 12:07:00 +0300

    Init Add mouse wheel copy Add hotkey copy Add negative values support Add manifest

---
+++ src/main.ts
@@ -0,0 +1,39 @@
+addEventListener('mousedown', e => {
+    if (e.button !== 1 || !e.altKey) return;
+    e.preventDefault();
+
+    const text = (e.target as HTMLElement).innerText || (e.target as HTMLInputElement).value;
+    if (text)
+        handleString(text, e.shiftKey);
+})
+
+addEventListener('keydown', e => {
+    if (e.code === 'KeyC' && e.altKey) {
+        const str = getSelectedText();
+        if (!str) return;
+
+        handleString(str, e.shiftKey);
+    }
+});
+
+function handleString(str: string, round: boolean) {
+    const pxRegex = /(-?\d+[.|,]*\d*)\W*[px]*/gim
+    const match = pxRegex.exec(str);
+
+    const numberInPx = match?.[1];
+    if (!numberInPx) return;
+
+    const dotNumber = numberInPx.replace(',', '.');
+    const px = Number(dotNumber);
+
+    let rem = px / 16;
+    const remStr = round
+        ? Math.round(rem).toString()
+        : rem.toFixed(2)
+
+    navigator.clipboard.writeText(`${remStr}rem`);
+}
+
+function getSelectedText() {
+    return window.getSelection()?.toString() ?? '';
+}
